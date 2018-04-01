b5fcf2c4c493a6bbc18411027ace73ce8db4347f
Евгений Свиридков <sviridkov.j@gmail.com>
2018-04-02 00:43:58 +0700

    Added hidden keyboard

--- TargetBox/ViewController.swift
+++ TargetBox/ViewController.swift
@@ -78,5 +78,10 @@ class ViewController: UIViewController {
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
 }
 
