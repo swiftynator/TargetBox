f9048f00a005fd4b18779d0d4befee023ecc8a69
Евгений Свиридков <sviridkov.j@gmail.com>
2018-04-01 22:54:08 +0700

    Added constraints and variables

--- TargetBox/ViewController.swift
+++ TargetBox/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hghTextField: UITextField!
+    @IBOutlet weak var bigBoxTextField: UITextField!
+    @IBOutlet weak var smallBoxTextField: UITextField!
+    
+    @IBOutlet weak var numberOfBoxLabel: UILabel!
+    
+    let valueBigBox = 93.15
+    let valueSmallBox = 16.32
+    let valueHghBox = 389.424
+    
+    let typeLBox = 19440.0
+    let typeMBox = 9000.0
+    let typeBBox = 21398.75
+    let typeVBox = 13323.75
+    let typeDBox = 3630.0
+    let typeEBox = 2227.5
+    let typeGBox = 2100.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +38,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateButton(_ sender: UIButton) {
+    }
+    
 }
 
