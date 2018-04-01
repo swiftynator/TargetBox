8f6a9e38f7e701d7659c0330222fdb7269512565
Евгений Свиридков <sviridkov.j@gmail.com>
2018-04-01 21:12:28 +0700

    Initial Commit

---
+++ TargetBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TargetBox
+//
+//  Created by Свиридков Евгений on 01.04.2018.
+//  Copyright © 2018 T-sterone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
